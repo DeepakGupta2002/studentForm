198b98a7dd4d9c599be09157da43d56705518348
deepak gupt <dg476504@gmail.com>
2024-04-14 02:24:30 +0530

    update the student controller code

--- formWithNodeJS/src/controller/studentController/stuController.js
+++ formWithNodeJS/src/controller/studentController/stuController.js
@@ -1,31 +1,39 @@
-
 const { table } = require("../../model/studentSchema");
-
+var rn = require('random-number');
+var options = {
+    min: -1000
+    , max: 1000
+    , integer: true
+}
 let postcontroller = async (req, res) => {
     console.log(req.body);
     let { studentName, studentEmail, studentPhone, studentDOB, studentGender, studentAddress } = req.body;
 
     console.log(req.body);
 
+    try {
+        await table.create({
+            studentName: studentName,
+            studentEmail: studentEmail,
+            studentPhone: studentPhone,
+            studentDOB: studentDOB,
+            studentGender: studentGender,
+            studentAddress: studentAddress,
+            key: rn(options)
 
-    let student_data = new table({
-        studentName: studentName,
-        studentEmail: studentEmail,
-        studentPhone: studentPhone,
-        studentDOB: studentDOB,
-        studentGender: studentGender,
-        studentAddress: studentAddress,
-    })
-
-    student_data.save()
-        .then(result => {
-            res.status(200).json({
-                message: 'Student form submitted successfully',
-                data: req.body
-            });
-        })
-        .catch(error => {
-            console.error('Error inserting data:', error);
         });
+
+        res.status(200).json({
+            message: 'Student form submitted successfully',
+            data: req.body
+        });
+    } catch (error) {
+        console.error('Error inserting data:', error);
+        res.status(500).json({
+            message: 'Error occurred while inserting data',
+            error: error
+        });
+    }
 }
+
 module.exports = { postcontroller };
