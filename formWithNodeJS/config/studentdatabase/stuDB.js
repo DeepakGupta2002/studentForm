75339b2d5c65009d398b822bf6c8f694f0ef16fb
deepak gupt <dg476504@gmail.com>
2024-04-14 02:34:42 +0530

    chage the congig file for database connection

--- formWithNodeJS/config/studentdatabase/stuDB.js
+++ formWithNodeJS/config/studentdatabase/stuDB.js
@@ -1,10 +1,15 @@
 require("dotenv/config");
 const mongoose = require("mongoose");
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.yupixxr.mongodb.net/`).then(() => {
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_NAME}.yupixxr.mongodb.net/`,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000
+}).then(() => {
     console.log("Connected to MongoDB");
 }).catch((err) => {
     console.log(err);
 });
 
 module.exports = { mongoose };
+
